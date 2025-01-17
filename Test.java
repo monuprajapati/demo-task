48539e87693d1454020336fcf000ffa48ad2c0c7
monuprajapati <143016982+monuprajapati@users.noreply.github.com>
2025-01-17 16:07:07 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,10 @@
+
+// This is a my simple Java program to print "Hello, World!"
+
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World! Monu here");
+    }
+}
